cc11d0fa1fc13e87d4ed2bb1c7b59c0afe85a21c
Jacob Smith <jasmith2016@yahoo.com>
2020-04-11 22:46:50 -0700

    Implement express

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const express = require('express')
 const sqlite3 = require('sqlite3').verbose()
 
+const app = express()
+const port = 3000
 const DB_PATH = './sqlite.db'
 
 const DB = new sqlite3.Database(DB_PATH, function(err) {
@@ -10,23 +13,22 @@ const DB = new sqlite3.Database(DB_PATH, function(err) {
 	console.log('Connected to ' + DB_PATH + ' database.')
 });
 
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/test', (req, res) => {
+	DB.get(`SELECT * FROM Users WHERE id = 1`, null, (err, row) => {
+		console.log(row)
+	})
+	res.send('Select complete')
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
 dbSchema = `
 CREATE TABLE IF NOT EXISTS Users (
-	id integer NOT NULL PRIMARY KEY,
-	login text NOT NULL UNIQUE,
-	password text NOT NULL UNIQUE,
-	email text NOT NULL UNIQUE,
-	first_name text,
-	last_name text
-);
-
-CREATE TABLE IF NOT EXISTS Blogs (
-	id integer NOT NULL PRIMARY KEY,
-	user_id integer NOT NULL UNIQUE,
-	blog text,
-	title text NOT NULL,
-	publish_data date,
-	FOREIGN KEY (user_id) REFERENCES Users (id)
+	id integer PRIMARY KEY,
+	user_name text,
+	is_active integer CHECK(is_active IN(0, 1))
 );`
 
 DB.exec(dbSchema, function(err) {
@@ -34,5 +36,16 @@ DB.exec(dbSchema, function(err) {
 		console.log(err)
 	}
 });
+
+DB.run(`INSERT INTO Users (user_name) VALUES ('george')`, null, (err, row) => {
+	if (err) {
+		console.log(err)
+	}
+	else {
+		console.log('INPUT Succeeded')
+	}
+})
+
+
 
 DB.close();
